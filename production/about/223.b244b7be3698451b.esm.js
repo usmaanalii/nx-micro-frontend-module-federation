5f4537a6e30d7c7d77513c14fbbf11a79e39f7a1
Usmaan Ali <usmaanalii@hotmail.co.uk>
2022-08-30 12:20:20 +0100

    add host and remote applications

---
+++ production/about/223.b244b7be3698451b.esm.js
@@ -0,0 +1,15 @@
+(self["webpackChunkabout"] = self["webpackChunkabout"] || []).push([[223],{
+
+/***/ 1223:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* reexport safe */ _app_app__WEBPACK_IMPORTED_MODULE_0__.Z)
+/* harmony export */ });
+/* harmony import */ var _app_app__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(3266);
+
+
+/***/ })
+
+}])
